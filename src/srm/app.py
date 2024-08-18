1bebc8d54efc4a434bb2fceba077a8e54050384b
Ian Thompson <ianiat11@gmail.com>
2024-08-17 22:38:00 -0500

    modified:   src/srm/app.py 	- Stubbing :callable:`update`.

--- src/srm/app.py
+++ src/srm/app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+
+def update():
+    """Update the min/max values of a slider.
+
+    Values are updated to reflect changes made in either of the other
+    two sliders. The totals of all three sliders must equal three (3)
+    "standard deviations".
+    """
+    ...
+
+
 profit_share = st.slider(
     "Profit Share", min_value=0.0, max_value=30.0, step=0.1
 )

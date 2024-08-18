028b3db08fd73e7a72b5e539992f0be46b8e510b
Ian Thompson <ianiat11@gmail.com>
2024-08-17 22:33:32 -0500

    modified:   src/srm/app.py 	- Add min, max, and step sizes to sliders.

--- src/srm/app.py
+++ src/srm/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-profit_share = st.slider("Profit Share")
-monthly_revenue = st.slider("Monthly Revenue")
-equity = st.slider("Equity")
+profit_share = st.slider(
+    "Profit Share", min_value=0.0, max_value=30.0, step=0.1
+)
+monthly_revenue = st.slider(
+    "Monthly Revenue", min_value=5_000, max_value=20_000, step=100
+)
+equity = st.slider("Equity", min_value=0.0, max_value=21.0, step=0.1)

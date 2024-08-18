3539755a0e3890abc3a00dc4c4dd6a1623b644b9
Ian Thompson <ianiat11@gmail.com>
2024-08-18 00:35:10 -0500

    modified:   src/srm/app.py 	- Added logic to handle slider values dynamically.

--- src/srm/app.py
+++ src/srm/app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+MIN_PROFIT = 0.0
+MAX_PROFIT = 30.0
+MIN_REVENUE = 5_000
+MAX_REVENUE = 20_000
+RANGE_REVENUE = MAX_REVENUE - MIN_REVENUE
+MIN_EQUITY = 0.0
+MAX_EQUITY = 21.0
+
 
 def update(key: str) -> None:
     """Update the min/max values of a slider.
@@ -10,46 +18,56 @@ def update(key: str) -> None:
     """
     match key:
         case "profit":
-            profit_ratio = st.session_state.profit / profit_share.max_value  # noqa: F841
-            st.session_state.revenue = ...
-            st.session_state.equity = ...
+            ratio = st.session_state.profit / MAX_PROFIT
+            st.session_state.revenue = (
+                RANGE_REVENUE * (1 - ratio) / 2 + MIN_REVENUE
+            )
+            st.session_state.equity = MAX_EQUITY * (1 - ratio) / 2
 
         case "revenue":
-            revenue_ratio = (  # noqa: F841
-                st.session_state.revenue / monthly_revenue.max_value
-            )
-            st.session_state.profit = ...
-            st.session_state.equity = ...
+            # Revenue does *not* start at 0. Need to reduce the value
+            # by the min and then normalize by the range.
+            ratio = (st.session_state.revenue - MIN_REVENUE) / RANGE_REVENUE
+            st.session_state.profit = MAX_PROFIT * (1 - ratio) / 2
+            st.session_state.equity = MAX_EQUITY * (1 - ratio) / 2
 
         case "equity":
-            equity_ratio = st.session_state.equity / equity.max_value  # noqa: F841
-            st.session_state.profit = ...
-            st.session_state.revenue = ...
+            ratio = st.session_state.equity / MAX_EQUITY
+            st.session_state.profit = MAX_PROFIT * (1 - ratio) / 2
+            st.session_state.revenue = (
+                RANGE_REVENUE * (1 - ratio) / 2 + MIN_REVENUE
+            )
 
 
 profit_share = st.slider(
     label="Profit Share",
-    min_value=0.0,
-    max_value=30.0,
-    value=[0.0, 30.0],
+    min_value=MIN_PROFIT,
+    max_value=MAX_PROFIT,
+    value=MAX_PROFIT * 1 / 3,
     step=0.1,
     format="%0.1f%%",
     key="profit",
+    on_change=update,
+    args=("profit",),
 )
 monthly_revenue = st.slider(
     label="Monthly Revenue",
-    min_value=5_000,
-    max_value=20_000,
-    value=[5_000, 20_000],
+    min_value=MIN_REVENUE,
+    max_value=MAX_REVENUE,
+    value=int(round((MAX_REVENUE - MIN_REVENUE) * 1 / 3, -3)) + MIN_REVENUE,
     step=100,
     key="revenue",
+    on_change=update,
+    args=("revenue",),
 )
 equity = st.slider(
     label="Equity",
-    min_value=0.0,
-    max_value=21.0,
-    value=[0.0, 21.0],
+    min_value=MIN_EQUITY,
+    max_value=MAX_EQUITY,
+    value=(MAX_EQUITY - MIN_EQUITY) * 1 / 3,
     step=0.1,
     format="%0.1f%%",
     key="equity",
+    on_change=update,
+    args=("equity",),
 )
